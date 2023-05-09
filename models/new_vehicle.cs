cd8ef6decde5487d57db772fd5d63c4ba64500b6
tarek1488 <105943642+tarek1488@users.noreply.github.com>
2023-05-09 09:56:42 +0300

    vehicles classes

---
+++ models/new_vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Car_Store.models
+{
+    public class new_vehicle : vehicles
+    {
+        [Required]
+        int vehicle_id;
+
+        [Required]
+        int Number_of_classes;
+
+        [Required]
+        int start_price;
+
+        [Required]
+        int end_price;
+    }
+}
