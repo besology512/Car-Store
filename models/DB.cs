4e57a065faa6d42ed7b989bcb04561d264256154
Mohamed Elsayed <65646207+modi161@users.noreply.github.com>
2023-05-15 21:34:08 +0300

    completing the merging process

--- models/DB.cs
+++ models/DB.cs
@@ -11,13 +11,13 @@ namespace Car_Store.models
 
         public DB()
         {
-            string conString = "Data Source=LAPTOP-8NJTOS7O;Initial Catalog=THECARSTORE;Integrated Security=True";
+            string conString = "Data Source=DESKTOP-KQT84LF\\MSSQLSERVER2023;Initial Catalog=THECARSTORE;Integrated Security=True";
             con = new SqlConnection(conString);
         }
-        
-        public void insertUser(string Fname, string Lname, string pass, string date, string email)
+
+        public void insertUser(string Fname, string Lname, string pass, string date, string email, string UserName)
         { //to return any data type
-            string query = "insert into CLIENT values ('"+UserName+"','"+Fname+ "', '" + Lname + "', '" + pass + "', '" + date + "', '" + email + "', 0)";
+            string query = "insert into CLIENT values ('" + UserName + "','" + Fname + "', '" + Lname + "', '" + pass + "', '" + date + "', '" + email + "', 0)";
             object type;
             try
             {
@@ -26,7 +26,211 @@ namespace Car_Store.models
                 type = sqlCommand.ExecuteNonQuery();
                 con.Close();
             }
-            catch (SqlException) { con.Close();}
+            catch (SqlException) { con.Close(); }
         }
+
+        public void insert_vechile(string Brand, int CC, string Color, int year_Model, string Gearing, string B_style, int price, int Km, int car_class)
+        {
+            string q = "INSERT INTO VEHICLE(Car_Status,Brand,CC_Rnage,Color,Year_Model,Gearing,Body_Style)" +
+                "VALUES('Used','" + Brand + "'," + CC + ",'" + Color + "'," + year_Model + ",'" + Gearing + "','" + B_style + "')";
+            DateTime currentDate = DateTime.Now;
+            string formattedDate = currentDate.ToString("yyyy-MM-dd");
+            string q2 = "INSERT INTO USED_VEHICLE VALUES(" + 1 + "," + Km + "," + price + ",'" + formattedDate + "'," + car_class + ")";
+            excute_nonQuery(q);
+            excute_nonQuery(q2);
+        }
+
+        private object Readtable(string Q)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(Q, con);
+                dt.Load(cmd.ExecuteReader());
+                con.Close();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                return ex;
+            }
+        }
+
+        private object getsinglevalue(string Q)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(Q, con);
+                object scalar = cmd.ExecuteScalar();
+                con.Close();
+                return scalar;
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                return ex;
+            }
+        }
+
+        private object excute_nonQuery(string Q)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(Q, con);
+                int num = cmd.ExecuteNonQuery();
+                con.Close();
+                return num;
+
+
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                return ex;
+
+            }
+        }
+
+
+
+        public void insertShowRoom(string Name, string City, string street, string phoneNumber)
+        { //to return any data type
+            string query = "insert into SHOWROOM values ('" + Name + "', '" + City + "', '" + street + "', '" + phoneNumber + "')";
+            object type;
+            try
+            {
+                con.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                type = sqlCommand.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (SqlException) { con.Close(); }
+        }
+        public object ReadAll(string tablename)
+        { //to return any data type
+            string query = "select * from " + tablename;
+            return FunctionReaderExecute(query);
+        }
+        public object ReadTuple(string Name, string tablename, string pKey)
+        { //to return any data type
+            string query = "select * " + " from " + tablename + " where " + pKey + " = " + "'" + Name + "';";
+            return FunctionReaderTupleExecute(query);
+        }
+        private object FunctionReaderExecute(string query)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                dt.Load(sqlCommand.ExecuteReader());
+                con.Close();
+                return dt;
+            }
+            catch (SqlException ex) { con.Close(); return ex; }
+
+        }
+
+        private object FunctionReaderTupleExecute(string query)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                dt.Load(sqlCommand.ExecuteReader());
+                con.Close();
+                return dt;
+            }
+            catch (SqlException ex) { con.Close(); return ex; }
+
+        }
+        public void updateTupleRoom(string PreviousName, string Name, string City, string street, string phoneNumber)
+        {
+            string query = "update SHOWROOM set Show_Room_Name = '" + Name + "', City = '" + City + "', STreet = '" + street + "', phoneNumber = '" + phoneNumber + "' where Show_Room_Name = '" + PreviousName + "';";
+            try
+            {
+                con.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                sqlCommand.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (SqlException) { con.Close(); }
+        }
+        public void deletetuple(string tableName, string pKey, string pColumn)
+        {
+            string query = "delete from " + tableName + " where " + pColumn + " = '" + pKey + "';";
+            try
+            {
+                con.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                sqlCommand.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (SqlException ex) { con.Close(); }
+        }
+
+        // overloading if the primarykey is integer
+        public void deletetuple(string tableName, int pKey, string pColumn)
+        {
+            string query = "delete from " + tableName + " where " + pColumn + " =" + pKey + ";";
+            try
+            {
+                con.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                sqlCommand.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (SqlException ex) { con.Close(); }
+        }
+        public object getPasswordClient(string UserName)
+        { //to return any data type
+            string query = "select pass from CLIENT where Client_Username = '" + UserName + "';";
+            object pass;
+            try
+            {
+                con.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                pass = sqlCommand.ExecuteScalar();
+                if (pass == null)
+                {
+                    con.Close();
+                    return "notFound";
+                }
+                con.Close();
+                return (string)pass;
+            }
+            catch (SqlException) { con.Close(); return "notFound"; }
+        }
+
+        public object getPasswordEmployee(string UserName)
+        { //to return any data type
+            string query = "select pass from EMPLOYEE where Emp_Username = '" + UserName + "';";
+            object pass;
+            try
+            {
+                con.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                pass = sqlCommand.ExecuteScalar();
+                if (pass == null)
+                {
+                    con.Close();
+                    return "notFound";
+                }
+                con.Close();
+                return (string)pass;
+            }
+            catch (SqlException) { con.Close(); return "notFound"; }
+        }
+
+
+
     }
 }
+
