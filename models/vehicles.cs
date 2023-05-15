c71ee94c9e6a9ee441e6afeae0fa97d17ec5f02f
Ahmed Bassam <ahmedbassam513@gmail.com>
2023-05-15 23:14:52 +0300

    changes to the filter

--- models/vehicles.cs
+++ models/vehicles.cs
@@ -2,33 +2,60 @@
 
 namespace Car_Store.models
 {
-    public class vehicles
+    public class vehicle
     {
+        public vehicle()
+        {
+                
+        }
+        public vehicle(
+            string car_status = "",
+            string showroom = "",
+            string Brand = "",
+            int CC_Range = 0,
+            string color = "",
+            int year_model = 0,
+            string Gearing = "",
+            string Body_Style = "",
+            string car_image = "")
+        {
+            this.car_status = car_status;
+            this.showroom = showroom;
+            this.Brand = Brand;
+            this.CC_Range = CC_Range;
+            this.color = color;
+            this.year_model = year_model;
+            this.Gearing = Gearing;
+            this.Body_Style = Body_Style;
+            this.car_image = car_image;
+        }
+
+
         [Required, MaxLength(20)]
-        string car_status; // status of the car used,new 
+        public string car_status; // status of the car used,new 
 
         [MaxLength(100)]
-        string showroom;
+        public string showroom;
 
         [Required,MaxLength(50)]
-        string Brand;
+        public string Brand;
 
         [Required]
-        int CC_Range;
+        public int CC_Range;
 
         [Required,MaxLength(20)]
-        string color;
+        public string color;
 
         [Required]
-        int year_model;
+        public int year_model;
 
         [Required, MaxLength(20)]
-        string Gearing; // the car manual or automatic
+        public string Gearing; // the car manual or automatic
 
         [Required,MaxLength(20)]
-        string Body_Style; // style of the car --> suv,sedan,copeh
+        public string Body_Style; // style of the car --> suv,sedan,copeh
 
         [MaxLength(150)]
-        string car_image;
+        public string car_image;
     }
 }
