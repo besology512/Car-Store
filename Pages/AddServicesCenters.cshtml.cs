9c13eff74345dcc7de987df6dc33aa28f560432f
Mohamed Elsayed <65646207+modi161@users.noreply.github.com>
2023-05-16 17:09:32 +0300

    fixed some issues in display user (after updating schema) and added add Service center to the admin page

---
+++ Pages/AddServicesCenters.cshtml.cs
@@ -0,0 +1,36 @@
+using Car_Store.models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Car_Store.Pages.Shared
+{
+    public class AddServicesCentersModel : PageModel
+    {
+
+        public ServicesCenters myCenter { get; set; }
+        public IActionResult OnGet()
+        {
+
+            if (HttpContext.Session.GetInt32("User_Type") != 1)
+            {
+                return RedirectToPage("/index");
+            }
+            return Page();
+        }
+
+
+        public void OnPost(int id, string name, string address, string services, decimal latitude, decimal longitude, int stars)
+        {
+            myCenter = new ServicesCenters();
+            myCenter.ID = id;
+            myCenter.Name = name;
+            myCenter.Address = address; 
+            myCenter.Services = services;   
+            myCenter.latitude = latitude;
+            myCenter.longitude = longitude; 
+            myCenter.stars = stars;
+            myCenter.insert();
+
+        }
+    }
+}
