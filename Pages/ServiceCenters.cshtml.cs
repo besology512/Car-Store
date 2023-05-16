c72a32f87f4d93dc629b71b33c8839477c820f7b
Mohamed Elsayed <65646207+modi161@users.noreply.github.com>
2023-05-16 17:39:27 +0300

    now services centers page connected with the DB

--- Pages/ServiceCenters.cshtml.cs
+++ Pages/ServiceCenters.cshtml.cs
@@ -1,12 +1,19 @@
+using Car_Store.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
 
 namespace Car_Store.Pages
 {
     public class ServiceCentersModel : PageModel
     {
+        public ServicesCenters myCenter { get; set; }
+        public DataTable DT { get; set; }
+        public string mapsLink { get; set; }
         public void OnGet()
         {
+            myCenter = new ServicesCenters();
+            DT = (DataTable)myCenter.getAll();
         }
     }
 }
