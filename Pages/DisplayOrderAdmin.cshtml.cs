2efe0743ae4f1cde6103ed68178a46f89159f987
tarek1488 <105943642+tarek1488@users.noreply.github.com>
2023-05-20 19:20:27 +0300

    admin view and update order status

---
+++ Pages/DisplayOrderAdmin.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Car_Store.models;
+using System.Data;
+
+namespace Car_Store.Pages
+{
+    public class DisplayOrderAdminModel : PageModel
+    {
+        private readonly DB My_DB;
+
+        [BindProperty]
+        public DataTable dt { get; set; }
+
+        [BindProperty]
+        public int ordersNum { get; set; }
+        public DisplayOrderAdminModel(DB db)
+        {
+            My_DB = db;
+        }
+        public void OnGet()
+        {
+            dt = (DataTable)My_DB.get_all_orders_admin();
+            ordersNum = My_DB.get_num_of_order();
+        }
+        public IActionResult OnPost(string OrderStatus , int OrderId)
+        {
+            My_DB.update_order_status(OrderStatus, OrderId);
+            return RedirectToPage("/DisplayOrderAdmin");
+        }
+    }
+}
